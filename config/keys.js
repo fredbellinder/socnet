ca0bb7090986b4e770cd87ee934e6d919a81932f
Fred Bellinder <fred.bellinder@chasacademy.se>
2019-04-23 17:20:39 +0200

    :construction: Add validation part 1

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
     mongoURI: `mongodb+srv://hoverboard-admin:zkkGGkkORw80PXyI@socnet-94evt.mongodb.net/test?retryWrites=true`,
+    secretOrKey: `secret`,
+
 }
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
+
 const app = express();
+
+// body parser middleware
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // DB Config
 const db = require('./config/keys').mongoURI;
 // DB Connection
@@ -16,8 +24,11 @@ mongoose
     .then(() => console.log('mongoDB connection successful!'))
     .catch((err) => { console.log(`Oh no! MongoDB connection fell through :( following error is reported: ${err}`) });
 
-app.get('/', (req, res) => res.send('Hello World'));
+// Passport middleware
+app.use(passport.initialize());
 
+//PAssport config
+require('./config/passport')(passport)
 // Use Routes
 
 app.use('/api/users', users);
